52f70418770c96752467f8a5d2303cfc8101ccf5
Ajay Thanki <akthanki5@gmail.com>
2025-01-25 23:27:09 +0530

    added new project

---
+++ src/lib/data.ts
@@ -0,0 +1,98 @@
+export const projects = [
+    {
+        title: "Tarba Patient Portal",
+        description: "Tarba Patient Portal is a Personalized Cancer Treatment Portal for the client where users login and manage their profile and upload their medical records.",
+        image: "/images/tarba-patient-portal.png",
+        tags: ["React", "Redux-Toolkit", "Framer Motion", "Tailwind"],
+        // githubUrl: "https://github.com/ajaythanki/nike-shoes",
+        // liveUrl: "https://ajaythanki.github.io/nike-shoes/",
+        features: [
+            "Responsive design",
+            "Animation effects",
+            "Product showcase",
+            "Performance optimized"
+        ]
+    },
+    {
+        title: "Full Stack Restaurant Portal",
+        description: "A Restaurant Portal for the client where users can find the deals nearby them.",
+        // image: "",
+        tags: ["React.js", "Node.js", "Express.js", "MongoDB", "Stripe", "Bootsrap", "MUI", "Redux", "JWT", "Firebase Push Notifications"],
+        features: [
+            "Admin Dashboard",
+            "User management",
+            "Merchant Dashboard",
+            "Restaurant verification",
+            "Secure Payment Integration",
+            "Deal Finding",
+            "User Reviews & Ratings",
+            "Table Booking, Menu and Analytics"
+        ]
+    },
+
+    {
+        title: "Full Stack Medicine Platform",
+        description: "Healthcare platform connecting patients with doctors for virtual consultations.",
+        // image: "",
+        tags: ["React.js", "Node.js", "Express.js", "MongoDB", "Stripe", "Bootstrap", "MUI", "Redux", "JWT", "Firebase Push Notifications"],
+        features: [
+            "Video & Chat consultations",
+            "User management",
+            "Secure Payment Integration",
+            "Appointment scheduling",
+            "Medical records",
+            "E-prescriptions"
+        ]
+    },
+    {
+        title: "Indian Panchang App Admin Dashboard and Backend API",
+        description: "Panchang management dashboard with features to manage mobile app's content and panchang data.",
+        tags: ["React.js", "Node.js", "Express.js", "MongoDB", "Bootstrap", "MUI", "Redux", "JWT",],
+        features: [
+            "Integration",
+            "Importing Bulk data",
+            "Event planning tools"
+        ]
+    },
+    {
+        title: "Nike Landing Page",
+        description: "Modern and responsive landing page design of Nike product to showcase my tailwind skills.",
+        image: "/images/nike-landing-page.jpg",
+        tags: ["React", "Framer Motion", "Tailwind"],
+        githubUrl: "https://github.com/ajaythanki/nike-shoes",
+        liveUrl: "https://ajaythanki.github.io/nike-shoes/",
+        features: [
+            "Responsive design",
+            "Animation effects",
+            "Product showcase",
+            "Performance optimized"
+        ]
+    },
+    {
+        title: "Business Advice",
+        description: "I have developed whole login & signup flow with integration of Zapier, Memberstack, Airtable as per clients requirement.",
+        image: "/images/business-advice.jpg",
+        tags: ["Webflow", "Zapier", "Memberstack", "Airtable", "Javascript"],
+        // githubUrl: "https://github.com/ajaythanki/nike-shoes",
+        liveUrl: "https://businessadvice.com.au/",
+        features: [
+            "Responsive design",
+            "Customized UI Development",
+            "Customized Forms"
+        ]
+    },
+    {
+        title: "Air Pollution",
+        description: "It's a Hugo website which is created from Bootstrap theme and all the data comes from csv API and it get updated every midnight and all the pages are dynamically generated through the cvs data API using Node and Gulp, it's deployed on GihHub Action and hosted on Netlify CMS.",
+        image: "/images/airpollution.jpg",
+        tags: ["Hugo", "Bootstrap", "Node.js", "Gulp", "Netlify"],
+        // githubUrl: "https://github.com/ajaythanki/nike-shoes",
+        liveUrl: "https://www.airpollution.io/",
+        features: [
+            "Responsive design",
+            "Air Pollution Forecast",
+            "Data Visualization",
+            "Performance optimized"
+        ]
+    },
+]
